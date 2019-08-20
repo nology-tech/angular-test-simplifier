7953b43986b491b971bd9c91ab7cf775a66ab206
Craig Livings <craig@nology.io>
2019-08-20 11:15:50 +0100

    Incremented version

--- src/spec/greet.spec.ts
+++ src/spec/greet.spec.ts
@@ -1,10 +1,6 @@
 import { TestBed } from '@angular/core/testing';
-import { greet } from '../index';
 
 describe('Initial Test', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
-  it('should greet', () => {
-    expect(greet('World')).toBe('Hello World');
-  });
 });
