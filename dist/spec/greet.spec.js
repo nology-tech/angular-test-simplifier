e6d4e56051bc315afaad8f04b55d90a54c34f1ac
Craig Livings <craig@nology.io>
2019-08-20 08:50:49 +0100

    Added jasmine tests

---
+++ dist/spec/greet.spec.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var testing_1 = require("@angular/core/testing");
+var index_1 = require("../index");
+describe('Initial Test', function () {
+    beforeEach(function () { return testing_1.TestBed.configureTestingModule({}); });
+    it('should greet', function () {
+        expect(index_1.greet()).toBe('Hello World');
+    });
+});
---
+++ src/spec/greet.spec.ts
@@ -0,0 +1,10 @@
+import { TestBed } from '@angular/core/testing';
+import { greet } from '../index';
+
+describe('Initial Test', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it('should greet', () => {
+    expect(greet()).toBe('Hello World');
+  });
+});
