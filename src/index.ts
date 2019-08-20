0f0e600735eb6c231b2ea14a667d872177904629
Shea.Murphy <shea@nology.io>
2019-08-20 14:11:02 +0100

    Modified export to export as class

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,2 @@
-import { Component } from './component';
-import { WrappedComponent } from './integration';
-
-export const TestComponent = Component;
-export const IntegrationComponent = WrappedComponent;
+export { Component as TestComponent } from './component';
+export { WrappedComponent as IntegrationComponent } from './integration';
